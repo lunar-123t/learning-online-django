9e4c8c166966dcd0e036556dc41848e81405ae29
nguyenhuuchinh <chinhnguyenhuu2305@gmail.com>
2022-09-11 17:09:52 +0700

    no message

--- auth/api/urls.py
+++ auth/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
 from auth.api.views import CreateUserView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='auth_register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+# # xác minh ma thong bao
+#     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
