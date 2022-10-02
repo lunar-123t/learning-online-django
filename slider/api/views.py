bc9247a192cf4324c1edcdf62699ab4840f0552f
nguyenhuuchinh <chinhnguyenhuu2305@gmail.com>
2022-10-02 16:37:30 +0700

    no message

--- slider/api/views.py
+++ slider/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+
 from slider.api.serializers import SliderSerializer
 from slider.models import Slider
 
 
 class SliderApiView(ListAPIView):
+    permission_classes = [AllowAny]
     serializer_class = SliderSerializer
     queryset = Slider.objects.all().order_by('-order')
