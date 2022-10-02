264701be45f9cda0cd02930ee9b6363ee35fb81e
nguyenhuuchinh <chinhnguyenhuu2305@gmail.com>
2022-10-02 15:30:03 +0700

    no message

--- khoahoc/api/urls.py
+++ khoahoc/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('listkhoahoc', views.KhoahocApiView.as_view(), name='khoahoc'),
     path('listlevel', views.Listlevel.as_view(), name='level'),
     path('listalllevel', views.ListAlllevel.as_view(), name='levelall'),
+    path('search', views.SearchList.as_view(), name='levelall'),
 ]
--- khoahoc/api/views.py
+++ khoahoc/api/views.py
@@ -13,8 +13,10 @@ class KhoahocApiView(ListAPIView):
     serializer_class = KhoahocSerializer
     queryset = KhoaHoc.objects.all()
 
+
 class Listlevel(ListAPIView):
     serializer_class = LevelSerializer
+
     def get_queryset(self):
         dataguilen = self.request.data
         return LevelKhoaHoc.objects.filter(khoa_hoc_id=dataguilen['idkhoahoc'])
@@ -27,3 +29,15 @@ class ListAlllevel(ListAPIView):
         dataguilen = self.request.data
         return MonHoc.objects.filter(level_id=dataguilen['idlevel'])
 
+
+class SearchList(ListAPIView):
+    serializer_class = MonhocSerializer
+
+    def get_queryset(self):
+        data = self.request.data
+        ten_mon=data.get('ten_mon', "")
+        level_id=data.get('level_id', "")
+        print(ten_mon)
+        print(level_id)
+
+        return MonHoc.objects.filter(ten_mon_hoc__contains=ten_mon,level_id=level_id)
