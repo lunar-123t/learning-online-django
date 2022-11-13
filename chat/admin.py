eca673238d7a0339dc4eba8bae0078f5fc36aca8
nguyenhuuchinh <chinhnguyenhuu2305@gmail.com>
2022-11-13 15:12:06 +0700

    chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,40 @@
+import datetime
+import os
+
 from django.contrib import admin
 
-from chat.models import Chat
+from auth import models
+from auth.api import serializers
+from chat.models import Chat, AdminAnswer
 
 
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
+    exclude = ('id_create',)
     list_display = ("user",)
+
+    def delete_model(self, request, obj):
+        object=obj.id
+        super().delete_model(request, obj)
+        AdminAnswer.objects.filter(id=object).delete()
+
+@admin.register(AdminAnswer)
+class AdminAnswerView(admin.ModelAdmin):
+    def save_model(self, request, obj:AdminAnswer, form, change):
+        super(AdminAnswerView, self).save_model(request,obj, form, change)
+        user=obj.user
+        message_text=obj.message_text
+        id=obj.id
+        Chat.objects.create(
+                user=user,
+                message_text=message_text,
+                is_my_messages=False,
+                message_date=datetime.datetime.now(),
+                id=id
+            )
+    def delete_model(self, request, obj):
+        object=obj.id
+        super().delete_model(request, obj)
+        Chat.objects.filter(id=object).delete()
+
+
