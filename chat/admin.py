5e403fddcbe12c72041b482f5b7c651edda235e3
nguyenhuuchinh <chinhnguyenhuu2305@gmail.com>
2022-11-20 15:55:28 +0700

    vcv

--- chat/admin.py
+++ chat/admin.py
@@ -24,17 +24,17 @@ class AdminAnswerView(admin.ModelAdmin):
         super(AdminAnswerView, self).save_model(request,obj, form, change)
         user=obj.user
         message_text=obj.message_text
-        id=obj.id
+        print(obj)
         Chat.objects.create(
                 user=user,
                 message_text=message_text,
                 is_my_messages=False,
                 message_date=datetime.datetime.now(),
-                id=id
-            )
+                id_create=obj
+        )
     def delete_model(self, request, obj):
-        object=obj.id
+        id=obj.id
         super().delete_model(request, obj)
-        Chat.objects.filter(id=object).delete()
+        Chat.objects.filter(id_create=id).delete()
 
 
